d5a03201b8b0bd3dd6f6cd914a7089294b1b67a0
samhoog <samuel.hoogewind@icloud.com>
2023-09-18 22:48:12 -0400

    very early login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { GravatarModule} from "ngx-gravatar";
-import {NgxSpinnerModule} from "ngx-spinner";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { GravatarModule } from "ngx-gravatar";
+import { NgxSpinnerModule } from "ngx-spinner";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { LoginComponent } from './components/login/login.component';
 import { StudentDashboardComponent } from './components/home/student-dashboard/student-dashboard.component';
 import { ProfessorDashboardComponent } from './components/home/professor-dashboard/professor-dashboard.component';
 import { AuthComponent } from './auth/auth.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     StudentDashboardComponent,
     ProfessorDashboardComponent,
-    AuthComponent
+    AuthComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,6 +28,7 @@ import { AuthComponent } from './auth/auth.component';
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 
-
+export interface User {
+  username: string;
+  password: string;
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent {
 
+  currentUser = {} as User;
+  onLoginSubmit() {
+    console.log(this.currentUser.username, this.currentUser.password);
+  }
 }
