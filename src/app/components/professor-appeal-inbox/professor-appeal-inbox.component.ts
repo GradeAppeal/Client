e0724be3a6c8a0367193af0489a665bb2fbc6ca7
Tyler Voss <tylervoss101@gmail.com>
2023-09-25 17:00:14 -0400

    Adding avatar, working on appeal inbox

--- src/app/components/professor-appeal-inbox/professor-appeal-inbox.component.ts
+++ src/app/components/professor-appeal-inbox/professor-appeal-inbox.component.ts
@@ -1,4 +1,22 @@
+import { ViewEncapsulation } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SupabaseService } from 'src/app/services/supabase.service';
+
+export interface AppealInbox {
+  prefix: string,            //course prefix
+  code: number,              //course number
+  student_id: number,         
+  student_name: string,
+  assignment_id: number,
+  appeal_id: number,
+  created_at: Date,
+  appeal_text: string,
+  is_open: boolean
+}
+export interface Courses {
+  
+}
 @Component({
   selector: 'app-professor-appeal-inbox',
   templateUrl: './professor-appeal-inbox.component.html',
@@ -7,29 +25,40 @@ import { Component } from '@angular/core';
 
 
 export class ProfessorAppealInboxComponent {
+  inboxAppeals: AppealInbox[];
   appeals: any[];
-
   appeal: any;
   selectedAppeal: any;
+  date = new Date();
   appeal1 = {
-    name: 'Bob',
+    name: 'Bob Bubby',
     course_name: 'Math',
+    section: 'B',
     assignment_id: 'Test1',
     grade_received: 'F',
+    message_text: 'HELP I FAILED PLEASE DON:T FAIL ME',
+    email: "bobbubby@gmail.com"
   }
   appeal2 = {
-    name: 'Sam',
+    name: 'Sam Hoogewind',
     course_name: 'Stats',
+    section: 'A',
     assignment_id: 'Quiz1',
     grade_received: 'F',
+    message_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat ac felis donec et odio pellentesque diam volutpat. At lectus urna duis convallis convallis. Sagittis purus sit amet volutpat consequat mauris nunc congue nisi. Et netus et malesuada fames ac turpis. Egestas pretium aenean pharetra magna ac placerat vestibulum. Pharetra convallis posuere morbi leo urna molestie at elementum. Pretium quam vulputate dignissim suspendisse. Adipiscing diam donec adipiscing tristique risus nec feugiat in fermentum. Convallis convallis tellus id interdum velit laoreet. Natoque penatibus et magnis dis parturient montes.',
+    email: "samhoogewind@gmail.com"
   }
   appeal3 = {
-    name: 'Justin',
+    name: 'Justin Voss',
     course_name: 'CS112',
+    section: 'C',
     assignment_id: 'Test4',
     grade_received: 'F',
+    message_text: 'I failed the test. Let me redo it. Now.',
+    email: "jmanvoss1@gmail.com"
   }
-  constructor() {
+  constructor(private readonly supabase: SupabaseService, private router: Router) {
+    this.inboxAppeals = [];
     this.appeals = [this.appeal1, this.appeal2, this.appeal3];
     this.appeal = {
       name: '',
@@ -44,12 +73,15 @@ export class ProfessorAppealInboxComponent {
       grade_received: null,
     }
   }
-  // Function to select an appeal for editing
+  async ngOnInit(): Promise<void> {
+    //WAITING to add this until supabase is set up
+    // this.inboxAppeals = await this.supabase.fetchInboxAppeals(1);
+    // console.log(this.inboxAppeals);
+    // encapsulation: ViewEncapsulation.None;
+  }
+  // Function to select an appeal
   selectAppeal(appeal: any) {
     // Copy the selected appeal's data into the form fields
     this.selectedAppeal = appeal
-  }
-  composeMessage(){
-
   }
 }
