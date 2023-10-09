90a06f6edba24744ed2c62bdd8d64348e69af61e
Tyler Voss <tylervoss101@gmail.com>
2023-10-09 16:54:37 -0400

    fixing chat/database feature

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -63,11 +63,18 @@ export class ChatComponent {
   constructor(private supabase: SupabaseService) {}
   async ngOnInit() {
     this.user.id = 10; //TODO make this actual user ID not just fake data
+    this.professorAppeals = await this.supabase.fetchProfessorAppeals(1);
     if (this.current_appeal){
-      this.sender.id = this.current_appeal.student_id;
+      //this.sender.id = this.current_appeal.student_id;
       this.messages = await this.supabase.fetchMessages(this.current_appeal.appeal_id);
     }
-    this.professorAppeals = await this.supabase.fetchProfessorAppeals(1);
+    else{
+      this.current_appeal = this.professorAppeals[0];
+      this.messages = await this.supabase.fetchMessages(this.current_appeal.appeal_id);
+    }
+    this.messageCount = this.messages.length;
+    
+
   }
 
 
@@ -88,7 +95,7 @@ export class ChatComponent {
     async selectAppeal(appeal: any) {
       // Copy the selected appeal's data into the form fields
       this.current_appeal = appeal;
-      this.sender.id = this.current_appeal.student_id;
+      //this.sender.id = this.current_appeal.student_id;
       this.messages = await this.supabase.fetchMessages(this.current_appeal.appeal_id);
       console.log(this.current_appeal);
     }
@@ -106,35 +113,30 @@ export class ChatComponent {
    */
   async sendMessage(): Promise<void> {
     const now = getTimestampTz(new Date());
+
     try {
+      console.log(this.current_appeal.student_id);
+      let recipient_user_id = await this.supabase.getUserId(this.current_appeal.student_id, 'student');
+      console.log(recipient_user_id);
       await this.supabase.insertMessages(
         this.current_appeal.appeal_id,
         this.user.id,
-        3,
-        //this.current_appeal.student_id,
+        recipient_user_id,
         now,
         this.chatInputMessage,
         this.fromGrader
       );
       console.log("Sent to database!");
-      console.log(        this.current_appeal.appeal_id,
-        this.user.id,
-        this.current_appeal.student_id,
-        now,
-        this.chatInputMessage,
-        this.fromGrader);
-
         this.messages.push({
           id: 1 + this.messageCount,     //TODO make id better system
           created_at: getTimestampTz(new Date()), 
           sender_id: this.user.id, 
-          recipient_id: this.current_appeal.student_id, 
+          recipient_id: recipient_user_id, 
           appeal_id: this.current_appeal.appeal_id, 
           message_text: this.chatInputMessage, 
           from_grader: this.fromGrader
         });
     
-        this.messageCount ++;
         this.chatInputMessage = '';
         this.scrollToBottom();
         console.log(this.messages);
