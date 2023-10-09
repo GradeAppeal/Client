3bd1188f2f7a3e25cd1d0dd36c5f22b746361a74
Ael <bluebayband@gmail.com>
2023-10-09 15:49:39 -0400

    insert students and profs with trigger

--- src/app/components/professor-appeal-inbox/professor-appeal-inbox.component.ts
+++ src/app/components/professor-appeal-inbox/professor-appeal-inbox.component.ts
@@ -1,27 +1,9 @@
-import { ViewEncapsulation } from '@angular/compiler';
 import { Component, Output, EventEmitter } from '@angular/core';
 import { SupabaseService } from 'src/app/services/supabase.service';
 import {
   ProfessorAppeal,
   ProfessorCourse,
 } from 'src/app/shared/interfaces/professor.interface';
-
-interface CombinedData {
-  code: number;
-  id: number;
-  name: string;
-  prefix: string;
-  section: string;
-  semester: 'FA' | 'SP' | 'SU';
-  year: number;
-  appeal_id: number;
-  appeal_text: string;
-  assignment_id: number;
-  created_at: Date;
-  is_open: boolean;
-  student_id: number;
-  student_name: string;
-}
 
 @Component({
   selector: 'app-professor-appeal-inbox',
@@ -36,27 +18,6 @@ export class ProfessorAppealInboxComponent {
   email = 'abc123@gmail.com';
   showChat: boolean = false;
   date = new Date();
-  appeal1 = {
-    name: 'Bob Bubby',
-    course_name: 'Math',
-    section: 'B',
-    assignment_id: 'Test1',
-    grade_received: 'F',
-  };
-  appeal2 = {
-    name: 'Sam Hoogewind',
-    course_name: 'Stats',
-    section: 'A',
-    assignment_id: 'Quiz1',
-    grade_received: 'F',
-  };
-  appeal3 = {
-    name: 'Justin Voss',
-    course_name: 'CS112',
-    section: 'C',
-    assignment_id: 'Test4',
-    grade_received: 'F',
-  };
   professorAppeals!: ProfessorAppeal[];
   professorCourse!: ProfessorCourse[];
   selectedAppeal: ProfessorAppeal;
@@ -69,12 +30,17 @@ export class ProfessorAppealInboxComponent {
       this.professorCourse = await this.supabase.fetchProfessorCourses(1);
       this.selectedAppeal = this.professorAppeals[0];
       this.fetchedAppeals = true;
-      this.fixDate();
     } catch (err) {
       console.log(err);
     }
   }
-  fixDate() {}
+
+  formatTimestamp(timestamp: Date): { date: string; time: string } {
+    const d = new Date(timestamp);
+    const date = d.toDateString();
+    const time = d.toTimeString().split(' ')[0];
+    return { date, time };
+  }
 
   // Function to select an appeal
   selectAppeal(appeal: any) {
