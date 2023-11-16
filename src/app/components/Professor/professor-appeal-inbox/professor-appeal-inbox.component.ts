4265a6eee4ee9eda3ffde7fc2ed6129d91c46534
justinianvoss22 <jmanvoss@gmail.com>
2023-11-16 10:59:28 -0500

    assign grader in inbox

--- src/app/components/Professor/professor-appeal-inbox/professor-appeal-inbox.component.ts
+++ src/app/components/Professor/professor-appeal-inbox/professor-appeal-inbox.component.ts
@@ -16,6 +16,10 @@ import { formatTimestamp } from 'src/app/shared/functions/general.util';
 import { Session, User } from '@supabase/supabase-js';
 import { SignoutComponent } from '../../Auth/signout/signout.component';
 import { CloseAppealPopupComponent } from './close-appeal-popup/close-appeal-popup.component';
+import { AssignGraderPopupComponent } from '../professor-interaction-history/assign-grader-popup/assign-grader-popup.component';
+import { GraderAssignedSnackbarComponent } from '../professor-interaction-history/grader-assigned-snackbar/grader-assigned-snackbar.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-professor-appeal-inbox',
   templateUrl: './professor-appeal-inbox.component.html',
@@ -37,12 +41,13 @@ export class ProfessorAppealInboxComponent implements OnInit, OnChanges {
   professorCourses!: Course[];
   currentAppeal: ProfessorAppeal;
   fetchedAppeals = false;
-
+  durationInSeconds: number = 2;
   constructor(
     private router: Router,
     private dialog: MatDialog,
     private authService: SupabaseService,
-    private professorService: ProfessorService
+    private professorService: ProfessorService,
+    private _snackBar: MatSnackBar,
   ) {}
   async ngOnInit(): Promise<void> {
     try {
@@ -64,9 +69,12 @@ export class ProfessorAppealInboxComponent implements OnInit, OnChanges {
     }
   }
   ngOnChanges(): void {}
+
   selectAppeal(appeal: any) {
     this.currentAppeal = appeal;
+    console.log(this.currentAppeal);
   }
+
   localFormatTimestamp(timestamp: Date): { date: string; time: string } {
     return formatTimestamp(timestamp);
   }
@@ -78,11 +86,12 @@ export class ProfessorAppealInboxComponent implements OnInit, OnChanges {
     });
     //this.navigateTo('professor/closed-appeals/');
   }
+  
   navigateTo(route: string) {
     this.router.navigate([route]);
   }
 
-  async onCloseAppeal(e: MouseEvent) {
+  async onCloseAppeal(event: MouseEvent) {
     const currentAppeal = this.currentAppeal;
     const dialogRef = this.dialog.open(CloseAppealPopupComponent, {
       width: '30%',
@@ -97,5 +106,29 @@ export class ProfessorAppealInboxComponent implements OnInit, OnChanges {
       );
       this.currentAppeal = this.professorAppeals[0];
     });
+  }
+
+  async onButtonClick(event: MouseEvent) {
+    const currentAppeal = this.currentAppeal;
+    if (!this.currentAppeal.grader_id) {
+      const graders = await this.professorService.getGraders(
+        this.currentAppeal.course_id
+      );
+        
+      console.log({ graders });
+      const appealID = this.currentAppeal.appeal_id;
+      // open popup to assign grader
+      const dialog = this.dialog.open(AssignGraderPopupComponent, {
+        width: '30%',
+        height: '35%',
+        data: { graders, appealID },
+      });
+    }
+    else {
+        console.log('appeal already assigned to grader');
+        this._snackBar.openFromComponent(GraderAssignedSnackbarComponent, {
+          duration: this.durationInSeconds * 1000,
+        });
+    }
   }
 }
