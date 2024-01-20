6a33f861cf3e7d884d213650ba83b8efdfaae488
Ael <bluebayband@gmail.com>
2024-01-20 13:44:50 -0500

    force type

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -24,8 +24,8 @@ export class AuthService {
   // Code referred from: https://supabase.com/blog/authentication-in-ionic-angular#creating-the-ionic-angular-app
   constructor() {
     this.supabase = createClient(
-      process.env['SUPABASE_URL'],
-      process.env['SUPABASE_SERVICE_ROLE_KEY']
+      process.env['SUPABASE_URL'] as string,
+      process.env['SUPABASE_SERVICE_ROLE_KEY'] as string
     );
 
     // create auth user subscription
