0934bacc300592be9b2bdb17f5d04c78b5dc8967
justinianvoss22 <jmanvoss@gmail.com>
2024-01-24 16:03:03 -0500

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -63,7 +63,6 @@ import { DeleteAppealPopupComponent } from './components/Professor/closed-appeal
 import { ProfessorEmailConfirmationComponent } from './components/Auth/professor-email-confirmation/professor-email-confirmation.component';
 import { CheckEmailPopupComponent } from './components/Auth/register/check-email-popup/check-email-popup.component';
 import { StudentSetPasswordComponent } from './components/Auth/student-set-password/student-set-password.component';
-
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 //import { UpdateTemplateComponent } from './components/Professor/edit-templates/update-template/update-template/update-template.component';
 import { AddStudentComponent } from './components/Professor/roster/add-student/add-student.component';
