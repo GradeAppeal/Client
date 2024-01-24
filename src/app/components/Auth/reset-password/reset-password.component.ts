bfbff43bc3469866c5116aedaf5cf47ebc460cf8
Ael <bluebayband@gmail.com>
2024-01-24 16:05:02 -0500

    fix

--- src/app/components/Auth/reset-password/reset-password.component.ts
+++ src/app/components/Auth/reset-password/reset-password.component.ts
@@ -9,7 +9,7 @@ import { AuthService } from 'src/app/services/auth.service';
 export class ResetPasswordComponent {
   constructor(private authService: AuthService) {
     this.authService.getCurrentUser().subscribe((user) => {
-      console.log({ user });
+      console.log({ user }, 'resetPassword');
     });
   }
 }
