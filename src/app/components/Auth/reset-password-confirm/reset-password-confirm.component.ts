21e08666d69f0405626cad6bb28f7ed392a43d21
Ael <bluebayband@gmail.com>
2024-02-21 13:05:11 -0500

    add debug message

--- src/app/components/Auth/reset-password-confirm/reset-password-confirm.component.ts
+++ src/app/components/Auth/reset-password-confirm/reset-password-confirm.component.ts
@@ -10,6 +10,7 @@ export class ResetPasswordConfirmComponent {
   resetURL: string | null;
   constructor(private route: ActivatedRoute) {
     this.route.queryParams.subscribe((params: Params) => {
+      console.log({ params });
       this.resetURL = params['confirmation_url'] || null;
       this.resetURL = decodeURIComponent(this.resetURL as string);
       // You can then use this.confirmationUrl as needed in your component
